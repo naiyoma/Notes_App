a5590c95d2feaf766aaa7bdda206716950e8ff8a
naiyoma <lankas.aurelia@gmail.com>
2022-03-23 16:04:14 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react"
+import Sidebar  from "./components/Sidebar";
+
 import './App.css';
 
 function App() {
+
+  const [notes, setNotes] = React.useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Sidebar
+        notes={notes}
+      />
+    </main>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Sidebar(props) {
+    return(
+        <section className="sidebar--panel">
+            <div className="sidepanel--header">
+                <h3>Notes</h3>
+            </div>
+        </section>
+    )
+}
+
+export default Sidebar
